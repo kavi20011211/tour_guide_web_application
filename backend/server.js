92b1195baeb96447a45fb227eb72f5756cd134ef
kavi20011211 <@Lk6985iop>
2025-01-15 20:55:18 +0530

    update server and vercel files

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,20 @@ connectDB();
 
 const app = express();
 
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// Routes
 app.use('/api/users', require('./routes/UserRoute'));
 app.use('/api/bookings', require('./routes/BookingRoute'));
 
+// Define root route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
+// Exporting the Express app as a serverless function for Vercel
 module.exports = (req, res) => {
-  app(req, res); 
+  app(req, res); // Pass the request and response to Express app to handle
 };
