6c0ff8932d9d01852a9e88396e2a15af5c8f9b2c
kavi20011211 <@Lk6985iop>
2025-01-15 20:52:32 +0530

    updated server.js - 2

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,21 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
-const connectDB = require('./config/db')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
-app.use('/api/users',require('./routes/UserRoute'))
-app.use('/api/bookings',require('./routes/BookingRoute'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT,() => console.log(`Server is connected on ${PORT}`))
+
+app.use('/api/users', require('./routes/UserRoute'));
+app.use('/api/bookings', require('./routes/BookingRoute'));
 
 
 module.exports = (req, res) => {
-    if (req.url === '/') {
-      res.status(200).send('Hello, world!');
-    } else {
-      res.status(404).send('Not Found');
-    }
-  };
+  app(req, res); 
+};
